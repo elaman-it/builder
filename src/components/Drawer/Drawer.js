92e64f2cbeb1789d8e93211deee9547e938df81d
Elaman Imashov <elaman@outlook.com>
2021-03-17 15:39:45 +0600

    Added Drawer component

---
+++ src/components/Drawer/Drawer.js
@@ -0,0 +1,14 @@
+import Logo from "../Logo/Logo";
+import Nav from "../Nav/Nav";
+import classes from "./Drawer.module.css";
+
+const Drawer = ({  }) => {
+  return (
+    <div className={classes.Drawer}>
+      <Logo />
+      <Nav />
+    </div>
+  );
+}
+ 
+export default Drawer;
