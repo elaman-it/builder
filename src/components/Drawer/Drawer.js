ebe816506ef9b8220df3ab7f4ce6f2174f44c056
Elaman Imashov <elaman@outlook.com>
2021-03-19 14:52:24 +0600

    Add Backdrop component

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,12 +1,16 @@
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
+import Backdrop from "../UI/Backdrop/Backdrop";
 import classes from "./Drawer.module.css";
 
 const Drawer = () => {
   return (
     <div className={classes.Drawer}>
-      <Logo />
-      <Nav />
+      <Backdrop />
+      <div className={classes.content}>
+        <Logo />
+        <Nav />
+      </div>
     </div>
   );
 }
---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,9 @@
+import classes from "./Backdrop.module.css";
+
+const Backdrop = () => {
+  return (
+    <div className={classes.Backdrop}></div>
+  );
+}
+
+export default Backdrop;
