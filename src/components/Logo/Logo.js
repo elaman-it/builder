21c2379e88c5407545554a0cad0a0cda0e6f7e53
Elaman Imashov <elaman@outlook.com>
2021-03-19 14:21:14 +0600

    Added logo file

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,11 @@
 import classes from "./Logo.module.css";
+import logo from "../../images/logo.svg";
 
 const Logo = () => {
   return (
-    <div className={classes.Logo}>Logo</div>
+    <div className={classes.Logo}>
+      <img src={logo} alt="Logo of the Pizza project" />
+    </div>
   );
 }
 
