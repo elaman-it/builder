4316e3ec7796605c83d8b51ebc61772ce3bd53a1
Elaman Imashov <elaman@outlook.com>
2021-04-16 14:38:02 +0600

    Add and use CHeckout component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Layout from "./components/Layout/Layout";
 import PizzaBuilder from "./components/PizzaBuilder/PizzaBuilder";
+import Checkout from "./components/Checkout/Checkout";
 
 import "./App.css";
+import { Redirect, Route, Switch } from "react-router";
 
 const App = () => {
   return (
     <div className="App">
       <Layout>
-        <PizzaBuilder />
+        <Switch>
+          <Route path="/" component={PizzaBuilder} exact />
+          <Route path="/checkout" component={Checkout} />
+          <Redirect to="/" />
+        </Switch>
       </Layout>
     </div>
   );
