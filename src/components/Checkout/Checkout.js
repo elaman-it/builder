af5d11ac7a61cb9ff2a281399d983e3822ea5376
Elaman Imashov <elaman@outlook.com>
2021-05-03 14:43:45 +0600

    Add Firebase to checkoutform

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,11 +1,35 @@
 import PizzaPreview from "../PizzaBuilder/PizzaPreview/PizzaPreview";
 import CheckoutForm from "./CheckoutForm/CheckoutForm";
 import classes from "./Checkout.module.css";
+import axios from "axios";
 
 
 const Checkout = ({ history }) => {
   function cancelCallback() {
     history.replace('/');
+  }
+
+  function submitCallback(event) {
+    const data = new FormData(event.target);
+
+    axios.post('https://builder-a51d0-default-rtdb.firebaseio.com/orders.json', {
+      name: data.get('name'),
+      address: data.get('address'),
+      phone: data.get('phone'),
+      ingredients: {
+        tomato: 10,
+        salami: 10,
+        redPepper: 10,
+        yellowPepper: 10,
+        greenOlive: 10,
+        blackOlive: 10,
+      },
+      price: 100,
+    }).then(response => {
+      history.replace('/');
+    });
+
+    event.preventDefault();
   }
 
   return (
@@ -15,7 +39,9 @@ const Checkout = ({ history }) => {
         salami: 10,
         greenOlive: 50,
       }} price={150} />
-      <CheckoutForm />
+      <CheckoutForm
+        cancelCallback={cancelCallback}
+        submitCallback={submitCallback} />
     </div>
   );
 }
