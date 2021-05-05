b2e05175251e8167b0336b0001ac11d3c27766d9
Elaman Imashov <elaman@outlook.com>
2021-05-05 14:54:11 +0600

    Simplify reducer

--- src/redux/reducers/todo.js
+++ src/redux/reducers/todo.js
@@ -2,23 +2,30 @@ import { ADD_TODO, REMOVE_TODO } from "../actions/types";
 
 const initialState = {};
 
-const todo = (state = initialState, action) => {
+const addTodo = (state, action) => {
   const newState = { ...state };
+  newState[
+    (Math.random() * 9999999999).toFixed(0)
+  ] = action.text;
+  return newState;
+};
 
+const removeTodo = (state, action) => {
+  const newState = { ...state };
+  delete newState[action.id];
+  return newState;
+};
+
+const todo = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODO:
-      const id = Math.random() * 9999999999;
-      newState[id.toFixed(0)] = action.text;
-      break;
+      return addTodo(state, action);
     case REMOVE_TODO:
-      delete newState[action.id];
-      break;
+      return removeTodo(state, action);
   
     default:
-      break;
+      return state;
   }
-
-  return newState;
 }
 
 export default todo;
