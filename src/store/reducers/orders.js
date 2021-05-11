332ab1370e2cdb17de754e9f6b74460c2864a8e7
Elaman Imashov <elaman@outlook.com>
2021-05-11 15:10:07 +0600

    Use redux for orders

--- src/store/reducers/orders.js
+++ src/store/reducers/orders.js
@@ -1,6 +1,21 @@
+import { SET_ORDERS } from "../actions/types";
+
 const initialState = [];
 
 const orders = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_ORDERS:
+      return Object.keys(action.data).map(id => {
+        return {
+          ...action.data[id],
+          id: id,
+        };
+      });;
+  
+    default:
+      break;
+  }
+
   return state;
 }
 
